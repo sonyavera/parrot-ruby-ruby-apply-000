9ccde4db2e6e8f0e5b8013aa29bab1d777cb88e0
Sonya Gould <sonyavgould@gmail.com>
2020-05-19 21:25:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
-  puts "parrot"
-  returns "parrot"
+def parrot(parrot="Squawk!")
+  puts "{#{parrot}}"
+  puts parrot
 end
-
-parrot
